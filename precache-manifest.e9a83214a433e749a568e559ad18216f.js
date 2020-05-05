eba9dbc48d1e19e4225024c51553bbc3c8057870
DevHao06 <jiahaowang09@gmail.com>
2020-05-05 01:37:42 -0400

    Updates

---
+++ precache-manifest.e9a83214a433e749a568e559ad18216f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dc3fb85f41207c37267d36bb4b8db7",
+    "url": "/recipe-app.git/index.html"
+  },
+  {
+    "revision": "76456db44622f4e5ef3b",
+    "url": "/recipe-app.git/static/css/main.910a9eeb.chunk.css"
+  },
+  {
+    "revision": "a0f94646664ccea0501a",
+    "url": "/recipe-app.git/static/js/2.07029cd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app.git/static/js/2.07029cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76456db44622f4e5ef3b",
+    "url": "/recipe-app.git/static/js/main.045fc920.chunk.js"
+  },
+  {
+    "revision": "3982d8fce1d7d68c80fd",
+    "url": "/recipe-app.git/static/js/runtime-main.d4674b33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DevHao06/recipe-app.git/precache-manifest.7c43515d08de5aee503af636f1e465f5.js"
+  "/recipe-app.git/precache-manifest.e9a83214a433e749a568e559ad18216f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DevHao06/recipe-app.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipe-app.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
